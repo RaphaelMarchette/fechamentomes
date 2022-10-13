dddbf87b16353dffb40eca97f5ca290616c7ae21
RaphaelMarchette <raphael.marchette23@gmail.com>
2022-10-12 21:46:53 -0300

    e

--- meu_site.py
+++ meu_site.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-# route -> hashtagtreinamentos.com/
-# função -> o que você quer exibir naquela página
-# template
 
 @app.route("/")
 def homepage():
@@ -13,9 +10,6 @@ def homepage():
 def contatos():
     return render_template("contatos.html")
 
-@app.route("/usuarios/<nome_usuario>")
-def usuarios(nome_usuario):
-    return render_template("usuarios.html", nome_usuario=nome_usuario)
 
 # colocar o site no ar
 if __name__ == "__main__":
